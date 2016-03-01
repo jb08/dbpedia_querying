7293486169d1af523f3db01b34afb8949e4d7246
Rosio <rosiomacedo@gmail.com>
2016-02-29 21:09:44 -0600

    Script now allows a user to enter the title of a book and the author is displayed

--- sparqlPython.py
+++ sparqlPython.py
@@ -6,22 +6,32 @@ def query():
 	"""
 	"""
 	title = input1.get()
+
 	print title
+
+	query = "SELECT ?book ?author WHERE {?book dbp:name \""+ title+ "\"@en . ?book dbp:author ?author }"
+	print "Query: " + query
 	sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-	sparql.setQuery("""
-	    SELECT ?book
-	    WHERE {?book dbp:pages 1216}
-	""")
+	sparql.setQuery(query)
 	sparql.setReturnFormat(JSON)
 	results = sparql.query().convert()
-	#print results
+	print results
 	res = ""
 	for result in results["results"]["bindings"]:
 	    res += (result["book"]["value"]) + "\n"
+	    print "Book Title: " + result["book"]["value"]
+	    author = result["author"]["value"]
+	    print "Book's Author: " + result["author"]["value"]
+
+	    print
+
+	author = author[author.rfind("/")+1:]
+	#author.split("/")[-1] another way
+	var.set(author)
 
 	tkMessageBox.showinfo("Query Results", res)
 
-
+author = ""
 top = tk.Tk()
 textFrame = tk.Frame(top)
 textFrame.pack(fill="x")
@@ -33,5 +43,13 @@ buttonFrame = tk.Frame(top)
 buttonFrame.pack(side="bottom")
 b1 = tk.Button(buttonFrame, text="Query", command= query)
 b1.pack(side="bottom", fill="x")
+
+bottomBottomFrame = tk.Frame(top)
+bottomBottomFrame.pack(side="bottom")
+var = tk.StringVar()
+var.set(author)
+ms = tk.Message(bottomBottomFrame, textvariable=var)
+ms.pack(side="bottom")
+
 top.mainloop()
 
