ac424335800d8f94c45d314bda7ef5cb4638a7bb
Rosio <rosiomacedo@gmail.com>
2016-03-02 15:06:00 -0600

    Latest, sorry it s messy

--- sparqlPython.py
+++ sparqlPython.py
@@ -1,13 +1,71 @@
 import Tkinter as tk
 import tkMessageBox
+import tkFont
 from SPARQLWrapper import SPARQLWrapper, JSON
+
+
+#Common novel properties
+prop = {"author": "dbp:author", "genre": "dbp:genre", "country": "dbp:country", "title":"dbp:name", "pages":"dbp:pages", "publisher":"dbp:publisher", "subjects":"dct:subject", "similar":"rdfs:seeAlso"}
+
+class App(tk.Frame):
+	def __init__(self, master = None):
+		tk.Frame.__init__(self, master)
+		self.grid(sticky=tk.N+tk.S+tk.E+tk.W)
+		self.labelFont = tkFont.Font(family='Times', size='16')
+		self.buttonFont = tkFont.Font(family='Times', size='18', weight='bold')
+		self.createWidget()
+
+	def createWidget(self):
+		
+		
+
+		top = self.winfo_toplevel()
+		top.geometry('300x400')
+		top.rowconfigure(0,weight=1)
+		top.columnconfigure(0,weight=1)
+		self.columnconfigure(0,weight=1)
+
+		top.rowconfigure(1,weight=3)
+		top.columnconfigure(1,weight=3)
+		self.columnconfigure(1,weight=3)
+
+		self.label1 = tk.Label(self, text="Title*", font=self.labelFont, anchor=tk.W)
+		self.label1.grid(column=0, row=0)
+		self.input1 = tk.Entry(self)
+		self.input1.grid(column=1,row=0, sticky=tk.E+tk.W)
+		self.b1 = tk.Button(self, text="Query", font=self.buttonFont, command= query)
+		self.b1.grid(column=0, row=2, columnspan=2, sticky=tk.N+tk.S+tk.E+tk.W)
+
+
+	def displayWidget(self, result):
+		var = tk.StringVar()
+		var.set(result)
+		self.authorLabel = tk.Label(self, text="Author", font=self.labelFont, anchor=tk.W)
+		self.authorLabel.grid(column=0, row=1)
+		self.disp = tk.Message(self, textvariable=var, anchor = tk.W)
+		self.disp.grid(column=1, row=1, sticky=tk.N+tk.S+tk.E+tk.W)
+
+
+def cleanInput():
+	"""
+	"""
+
+def cleanResults(res):
+	"""
+	"""
+	#get rid of namespace
+	return res[res.rfind("/")+1:]
+
+def buildQuery(subject, predicate, object):
+	"""
+	"""
+
 
 def query():
 	"""
 	"""
-	title = input1.get()
+	title = app.input1.get()
 
-	print title
 
 	query = "SELECT ?book ?author WHERE {?book dbp:name \""+ title+ "\"@en . ?book dbp:author ?author }"
 	print "Query: " + query
@@ -25,31 +83,19 @@ def query():
 
 	    print
 
-	author = author[author.rfind("/")+1:]
+	author = cleanResults(author)
+	#print author
 	#author.split("/")[-1] another way
-	var.set(author)
+	display = author
+	app.displayWidget(display);
 
-	tkMessageBox.showinfo("Query Results", res)
+	#tkMessageBox.showinfo("Query Results", res)
+
+
 
 author = ""
-top = tk.Tk()
-textFrame = tk.Frame(top)
-textFrame.pack(fill="x")
-label1 = tk.Label(textFrame, text="Title")
-label1.pack(side="left")
-input1 = tk.Entry(textFrame)
-input1.pack(side="right",fill="x")
-buttonFrame = tk.Frame(top)
-buttonFrame.pack(side="bottom")
-b1 = tk.Button(buttonFrame, text="Query", command= query)
-b1.pack(side="bottom", fill="x")
 
-bottomBottomFrame = tk.Frame(top)
-bottomBottomFrame.pack(side="bottom")
-var = tk.StringVar()
-var.set(author)
-ms = tk.Message(bottomBottomFrame, textvariable=var)
-ms.pack(side="bottom")
-
-top.mainloop()
-
+app = App()
+app.master.title('Query DBpedia')
+app.mainloop()
+ 
